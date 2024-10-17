9c14a59a069e86695d9c4c5d22278d2787148811
homura163 <niclouie@outlook.com>
2024-10-17 11:42:59 -0400

    Add field for domain name and clean up code

--- src/Plugin/search_api/processor/IslandoraDomainName.php
+++ src/Plugin/search_api/processor/IslandoraDomainName.php
@@ -13,16 +13,16 @@ use Drupal\Component\Utility\Html;
  * Adds the item's view count to the indexed data.
  *
  * @SearchApiProcessor(
- *   id = "islandora_site_name",
- *   label = @Translation("Islandora Site Name"),
- *   description = @Translation("Add index for title of an Islandora site"),
+ *   id = "islandora_domain_name",
+ *   label = @Translation("Islandora Domain Name"),
+ *   description = @Translation("Add index for title of an Islandora Domain"),
  *   stages = {
  *     "add_properties" = 0,
  *   },
  *   hidden = false,
  * )
  */
-class IslandoraSiteName extends ProcessorPluginBase {
+class IslandoraDomainName extends ProcessorPluginBase {
 
   /**
    * The HTTP client to fetch the feed data with.
@@ -54,15 +54,15 @@ class IslandoraSiteName extends ProcessorPluginBase {
    */
   public function getPropertyDefinitions(DatasourceInterface $datasource = NULL) {
     $properties = [];
-
     if (!$datasource) {
       $definition = [
-        'label' => $this->t('Islandora Site Name'),
-        'description' => $this->t('Name of Islandora Site to be indexed to Solr'),
+        'label' => $this->t('Islandora Domain Name'),
+        'description' => $this->t('Name of Islandora Domain to be indexed to Solr'),
         'type' => 'string',
         'processor_id' => $this->getPluginId(),
+//         'processor_id' => 'joijoin3i_3djn',
       ];
-      $properties['search_api_islandora_site_name'] = new ProcessorProperty($definition);
+      $properties['search_api_islandora_domain_name'] = new ProcessorProperty($definition);
     }
 
     return $properties;
@@ -74,15 +74,17 @@ class IslandoraSiteName extends ProcessorPluginBase {
   public function addFieldValues(ItemInterface $item) {
     $datasourceId = $item->getDatasourceId();
     if ($datasourceId == 'entity:node') {
-      $siteTitle = $this->configFactory->get('system.site')->get('name');
+      $domain = \Drupal::request()->getSchemeAndHttpHost();
       $fields = $this->getFieldsHelper()->filterForPropertyPath($item->getFields(), NULL,
-      'search_api_islandora_site_name');
+      'search_api_islandora_domain_name');
       foreach ($fields as $field) {
-        if ($siteTitle) {
-          $field->addValue($siteTitle);
+        if ($domain) {
+          $field->addValue($domain);
         }
       }
     }
   }
 
+
 }
+
