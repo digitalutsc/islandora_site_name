8486186e22b958f8577ff86774aa13c81839cb72
Aryan-R-Patel <aryanp.031206@gmail.com>
2026-03-25 11:58:18 -0400

    fix: use phpcbf to fix linting errors

--- src/Plugin/search_api/processor/IslandoraDomainName.php
+++ src/Plugin/search_api/processor/IslandoraDomainName.php
@@ -7,7 +7,6 @@ use Drupal\search_api\Item\ItemInterface;
 use Drupal\search_api\Processor\ProcessorPluginBase;
 use Drupal\search_api\Processor\ProcessorProperty;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\Component\Utility\Html;
 
 /**
  * Adds the item's view count to the indexed data.
@@ -22,85 +21,82 @@ use Drupal\Component\Utility\Html;
  *   hidden = false,
  * )
  */
-class IslandoraDomainName extends ProcessorPluginBase
-{
-    /**
-     * The HTTP client to fetch the feed data with.
-     *
-     * @var \GuzzleHttp\ClientInterface
-     */
-    protected $httpClient;
+class IslandoraDomainName extends ProcessorPluginBase {
+  /**
+   * The HTTP client to fetch the feed data with.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
+  protected $httpClient;
 
-    /**
-     * Theme settings config.
-     *
-     * @var \Drupal\Core\Config\ConfigFactoryInterface
-     */
-    protected $configFactory;
+  /**
+   * Theme settings config.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
 
-    /**
-     * {@inheritdoc}
-     */
-    public static function create(
-        ContainerInterface $container,
-        array $configuration,
-        $plugin_id,
-        $plugin_definition
-    ) {
-        /** @var static $processor */
-        $processor = parent::create(
-            $container,
-            $configuration,
-            $plugin_id,
-            $plugin_definition
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(
+    ContainerInterface $container,
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+  ) {
+    /** @var static $processor */
+    $processor = parent::create(
+          $container,
+          $configuration,
+          $plugin_id,
+          $plugin_definition
+      );
+    $processor->httpClient = $container->get("http_client");
+    $processor->configFactory = $container->get("config.factory");
+    return $processor;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPropertyDefinitions(
+    ?DatasourceInterface $datasource = NULL,
+  ) {
+    $properties = [];
+    if (!$datasource) {
+      $definition = [
+        "label" => $this->t("Islandora Domain Name"),
+        "description" => $this->t(
+                "Name of Islandora Domain to be indexed to Solr"
+            ),
+        "type" => "string",
+        "processor_id" => $this->getPluginId(),
+      ];
+      $properties["search_api_islandora_domain_name"] = new ProcessorProperty($definition);
+    }
+
+    return $properties;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function addFieldValues(ItemInterface $item) {
+    $datasourceId = $item->getDatasourceId();
+    if (in_array($datasourceId, ["entity:node", "entity:taxonomy_term"])) {
+      $domain = \Drupal::request()->getSchemeAndHttpHost();
+      $fields = $this->getFieldsHelper()->filterForPropertyPath(
+            $item->getFields(),
+            NULL,
+            "search_api_islandora_domain_name"
         );
-        $processor->httpClient = $container->get("http_client");
-        $processor->configFactory = $container->get("config.factory");
-        return $processor;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getPropertyDefinitions(
-        DatasourceInterface $datasource = null
-    ) {
-        $properties = [];
-        if (!$datasource) {
-            $definition = [
-                "label" => $this->t("Islandora Domain Name"),
-                "description" => $this->t(
-                    "Name of Islandora Domain to be indexed to Solr"
-                ),
-                "type" => "string",
-                "processor_id" => $this->getPluginId(),
-            ];
-            $properties[
-                "search_api_islandora_domain_name"
-            ] = new ProcessorProperty($definition);
+      foreach ($fields as $field) {
+        if ($domain) {
+          $field->addValue($domain);
         }
-
-        return $properties;
+      }
     }
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function addFieldValues(ItemInterface $item)
-    {
-        $datasourceId = $item->getDatasourceId();
-        if (in_array($datasourceId,["entity:node", "entity:taxonomy_term"] )) {
-            $domain = \Drupal::request()->getSchemeAndHttpHost();
-            $fields = $this->getFieldsHelper()->filterForPropertyPath(
-                $item->getFields(),
-                null,
-                "search_api_islandora_domain_name"
-            );
-            foreach ($fields as $field) {
-                if ($domain) {
-                    $field->addValue($domain);
-                }
-            }
-        }
-    }
 }
--- src/Plugin/search_api/processor/IslandoraSiteName.php
+++ src/Plugin/search_api/processor/IslandoraSiteName.php
@@ -7,7 +7,6 @@ use Drupal\search_api\Item\ItemInterface;
 use Drupal\search_api\Processor\ProcessorPluginBase;
 use Drupal\search_api\Processor\ProcessorProperty;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\Component\Utility\Html;
 
 /**
  * Adds the item's view count to the indexed data.
@@ -22,86 +21,83 @@ use Drupal\Component\Utility\Html;
  *   hidden = false,
  * )
  */
-class IslandoraSiteName extends ProcessorPluginBase
-{
-    /**
-     * The HTTP client to fetch the feed data with.
-     *
-     * @var \GuzzleHttp\ClientInterface
-     */
-    protected $httpClient;
+class IslandoraSiteName extends ProcessorPluginBase {
+  /**
+   * The HTTP client to fetch the feed data with.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
+  protected $httpClient;
 
-    /**
-     * Theme settings config.
-     *
-     * @var \Drupal\Core\Config\ConfigFactoryInterface
-     */
-    protected $configFactory;
+  /**
+   * Theme settings config.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
 
-    /**
-     * {@inheritdoc}
-     */
-    public static function create(
-        ContainerInterface $container,
-        array $configuration,
-        $plugin_id,
-        $plugin_definition
-    ) {
-        /** @var static $processor */
-        $processor = parent::create(
-            $container,
-            $configuration,
-            $plugin_id,
-            $plugin_definition
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(
+    ContainerInterface $container,
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+  ) {
+    /** @var static $processor */
+    $processor = parent::create(
+          $container,
+          $configuration,
+          $plugin_id,
+          $plugin_definition
+      );
+    $processor->httpClient = $container->get("http_client");
+    $processor->configFactory = $container->get("config.factory");
+    return $processor;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPropertyDefinitions(
+    ?DatasourceInterface $datasource = NULL,
+  ) {
+    $properties = [];
+
+    if (!$datasource) {
+      $definition = [
+        "label" => $this->t("Islandora Site Name"),
+        "description" => $this->t(
+                "Name of Islandora Site to be indexed to Solr"
+            ),
+        "type" => "string",
+        "processor_id" => $this->getPluginId(),
+      ];
+      $properties["search_api_islandora_site_name"] = new ProcessorProperty($definition);
+    }
+
+    return $properties;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function addFieldValues(ItemInterface $item) {
+    $datasourceId = $item->getDatasourceId();
+    if (in_array($datasourceId, ["entity:node", "entity:taxonomy_term"])) {
+      $siteTitle = $this->configFactory->get("system.site")->get("name");
+      $fields = $this->getFieldsHelper()->filterForPropertyPath(
+            $item->getFields(),
+            NULL,
+            "search_api_islandora_site_name"
         );
-        $processor->httpClient = $container->get("http_client");
-        $processor->configFactory = $container->get("config.factory");
-        return $processor;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getPropertyDefinitions(
-        DatasourceInterface $datasource = null
-    ) {
-        $properties = [];
-
-        if (!$datasource) {
-            $definition = [
-                "label" => $this->t("Islandora Site Name"),
-                "description" => $this->t(
-                    "Name of Islandora Site to be indexed to Solr"
-                ),
-                "type" => "string",
-                "processor_id" => $this->getPluginId(),
-            ];
-            $properties[
-                "search_api_islandora_site_name"
-            ] = new ProcessorProperty($definition);
+      foreach ($fields as $field) {
+        if ($siteTitle) {
+          $field->addValue($siteTitle);
         }
-
-        return $properties;
+      }
     }
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function addFieldValues(ItemInterface $item)
-    {
-        $datasourceId = $item->getDatasourceId();
-        if (in_array($datasourceId,["entity:node", "entity:taxonomy_term"] )) {
-            $siteTitle = $this->configFactory->get("system.site")->get("name");
-            $fields = $this->getFieldsHelper()->filterForPropertyPath(
-                $item->getFields(),
-                null,
-                "search_api_islandora_site_name"
-            );
-            foreach ($fields as $field) {
-                if ($siteTitle) {
-                    $field->addValue($siteTitle);
-                }
-            }
-        }
-    }
 }
